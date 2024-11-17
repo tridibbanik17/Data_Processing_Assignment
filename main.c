89949000c4fa0a3c716a42e078924998942a236b
Tridib Banik <banikt@mcmaster.ca>
2024-11-17 00:59:44 -0500

    Add makefile

---
+++ main.c
@@ -0,0 +1,24 @@
+
+/*Author: Tridib Banik, Student Number: 400514461, MacID: banikt       
+ *This C program calls read_data, clean_impute, clean_delete and output_data.
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void usage() {
+    printf("Usage: clean [-d]\n");
+}
+int main() {
+    usage();
+    // Call read_data
+
+    // Call clean_impute
+
+    // Call clean_delete
+
+    // Call output_data
+
+    
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+clean: clean.c
+	gcc -o clean clean.c
+
+main: main.c 
+	gcc -o main main.c
